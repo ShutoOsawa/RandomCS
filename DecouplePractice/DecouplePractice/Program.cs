276b9ac3d3dbdb53bc9749daf26e1468a162fc7b
Tofu <osawa.shuto@gmail.com>
2022-03-12 12:50:59 +0900

    added several

---
+++ DecouplePractice/DecouplePractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace DecouplePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SportsTeam st = new SportsTeam("sport","team1");
+            st.PrintTeamInfo();
+        }
+    }
+
+    public class SportsTeam
+    {
+        public string Sport { get; set; }
+        public string TeamName { get; set; }
+
+        public SportsTeam(string sport, string teamName)
+        {
+            Sport = sport;
+            TeamName = teamName;
+        }
+
+        public void PrintTeamInfo()
+        {
+            System.Console.WriteLine(Sport + " - " + TeamName);
+        }
+
+        public void UpdateTeam()
+        {
+            System.Console.Write("Enter new team name --> ");
+            var newName = Console.ReadLine();
+            TeamName = newName;
+        }
+    }
+}
