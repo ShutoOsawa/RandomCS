b18c7404fbebc3c8d4838b28ab2f7b4a08e1dcc0
Tofu <osawa.shuto@gmail.com>
2022-03-15 21:51:11 +0900

    added several examples

--- DecouplePractice/DecouplePractice/Program.cs
+++ DecouplePractice/DecouplePractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DecouplePractice
 {
@@ -6,17 +7,23 @@ namespace DecouplePractice
     {
         static void Main(string[] args)
         {
-            SportsTeam st = new SportsTeam("sport","team1");
+            var productionConsole = new ProductionConsole();
+
+            SportsTeam st = new SportsTeam("sport","team1",productionConsole);
             st.PrintTeamInfo();
+
+            st.UpdateTeam();
+            st.PrintTeamInfo();
+
         }
     }
 
-    public class SportsTeam
+    public class SimpleSportsTeam
     {
         public string Sport { get; set; }
         public string TeamName { get; set; }
 
-        public SportsTeam(string sport, string teamName)
+        public SimpleSportsTeam(string sport, string teamName)
         {
             Sport = sport;
             TeamName = teamName;
@@ -34,4 +41,88 @@ namespace DecouplePractice
             TeamName = newName;
         }
     }
+
+    public class SportsTeam
+    {
+        public string Sport { get; set; }
+        public string TeamName { get; set; }
+
+        private IConsole Console { get; }
+        public SportsTeam(string sport, string teamName, IConsole console)
+        {
+
+            if(console == null)
+            {
+                throw new ArgumentNullException(nameof(console));
+            }
+            Sport = sport;
+            TeamName = teamName;
+            Console = console;
+        }
+
+        public void PrintTeamInfo()
+        {
+            Console.WriteLine(Sport + " - " + TeamName);
+        }
+
+        public void UpdateTeam()
+        {
+            Console.Write("Enter new team name --> ");
+            var newName = Console.ReadLine();
+            TeamName = newName;
+        }
+    }
+
+    public interface IConsole
+    {
+        void Write(string value);
+        void WriteLine(string value);
+        string ReadLine();
+    }
+
+    public class ProductionConsole : IConsole
+    {
+        public void Write(string value)
+        {
+            System.Console.Write(value);
+        }
+
+        public void WriteLine(string value)
+        {
+            System.Console.WriteLine(value);
+        }
+
+        public string ReadLine()
+        {
+            return System.Console.ReadLine();
+        }
+    }
+
+    public class TestableConsole : IConsole
+    {
+
+        public TestableConsole()
+        {
+            WrittenLines = new List<string>();
+        }
+
+        public List<string> WrittenLines { get; set; }
+        public void Write(string value)
+        {
+            WrittenLines.Add(value);
+        }
+
+        public void WriteLine(string value)
+        {
+            WrittenLines.Add(value);
+        }
+
+        public string LineToRead { get; set; }
+
+        public string ReadLine()
+        {
+            return LineToRead;
+        }
+    }
+
 }
---
+++ EnumPractice/EnumPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EnumPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int command = 1005;
+            if(Enum.IsDefined(typeof(Commands),command)){
+                Console.WriteLine(command);
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+
+    public enum Commands:int
+    {
+        Command1 = 1001,
+        Command2 = 1002,
+        Command3 = 1003
+    }
+
+
+}
